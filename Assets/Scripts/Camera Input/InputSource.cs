5750193d9e227b08662a8b86604c833f6a81af34
notDiSE <apribyl789@gmail.com>
2026-04-22 16:48:17 +0200

    ShardMotion files restructure

--- Assets/Scripts/Camera Input/InputSource.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "InputSource", menuName = "Scriptable Objects/InputSource")]
-public abstract class InputSource : ScriptableObject
-{
-    public float nevim;
-}
