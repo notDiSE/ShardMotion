e68fd86d768dcba363ad483b6d59494ac8c76c93
notDiSE <apribyl789@gmail.com>
2026-05-02 17:52:29 +0200

    moved Relative Pose Math

---
+++ Assets/ShardMotion/Scripts/Calibration/RelativePoseMath.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace ShardMotion.Calibration
+{
+    public readonly struct RelativePose
+    {
+        public readonly Vector3 LocalPositionToB;
+        public readonly Quaternion LocalRotationToB;
+
+        public RelativePose(Vector3 localPositionToB, Quaternion localRotationToB)
+        {
+            LocalPositionToB = localPositionToB;
+            LocalRotationToB = localRotationToB;
+        }
+    }
+
+    public static class RelativePoseMath
+    {
+        public static RelativePose Capture(Transform a, Transform b)
+            => new RelativePose(
+                b.InverseTransformPoint(a.position),
+                Quaternion.Inverse(b.rotation) * a.rotation
+            );
+
+        public static void Apply(Transform a, Transform b, in RelativePose rel)
+        {
+            a.SetPositionAndRotation(
+                b.TransformPoint(rel.LocalPositionToB),
+                b.rotation * rel.LocalRotationToB
+            );
+        }
+    }
+}
