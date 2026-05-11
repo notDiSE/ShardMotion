dea6c193d9e9cd05f6b163ea4686894e109d5a78
notDiSE <apribyl789@gmail.com>
2026-05-11 19:36:16 +0200

    comments for Animation scripts

--- Assets/ShardMotion/Scripts/Animation/WriteAnim.cs
+++ Assets/ShardMotion/Scripts/Animation/WriteAnim.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 
 namespace ShardMotion.Animation
 {
+    /// <summary>
+    /// Used to write to animation clip
+    /// </summary>
     public static class WriteAnim
     {
         #if UNITY_EDITOR
@@ -19,22 +22,27 @@ namespace ShardMotion.Animation
             prevRot = Quaternion.identity;
         }
 
-        public static void AddKey(
-            AnimationClip clip,
-            Transform root,
-            Transform target,
-            Vector3 localPos,
-            Quaternion localRot,
-            float time)
+        /// <summary>
+        /// Add key to animation at given time
+        /// </summary>
+        /// <param name="clip">Animation clip, to add key to</param>
+        /// <param name="root">Root Transform of the recorder</param>
+        /// <param name="target">Transform to record</param>
+        /// <param name="localPos">position of object</param>
+        /// <param name="localRot">rotation of object</param>
+        /// <param name="time"> time in animation </param>
+        public static void AddKey(AnimationClip clip, Transform root, Transform target, Vector3 localPos, Quaternion localRot, float time)
         {
-            if (!clip || !root || !target) return;
+            if (!clip || !root || !target) return; // not valid
 
-            var path = AnimationUtility.CalculateTransformPath(target, root);
+            var path = AnimationUtility.CalculateTransformPath(target, root); // get the path to object from root transform
 
-            localRot = Quaternion.Normalize(localRot);
+            localRot = Quaternion.Normalize(localRot); // normalize rotation 
 
+            // The rotation did full rotation (animation would do 360, should be fixed instead)
             if (hasPrev && Quaternion.Dot(prevRot, localRot) < 0f)
             {
+                // quaternion gets flipped == same rotation
                 localRot.x = -localRot.x;
                 localRot.y = -localRot.y;
                 localRot.z = -localRot.z;
@@ -44,6 +52,7 @@ namespace ShardMotion.Animation
             prevRot = localRot;
             hasPrev = true;
 
+            // Add keys for position and rotation as curve
             AddKey(clip, EditorCurveBinding.FloatCurve(path, typeof(Transform), "localPosition.x"), time, localPos.x);
             AddKey(clip, EditorCurveBinding.FloatCurve(path, typeof(Transform), "localPosition.y"), time, localPos.y);
             AddKey(clip, EditorCurveBinding.FloatCurve(path, typeof(Transform), "localPosition.z"), time, localPos.z);
@@ -56,42 +65,67 @@ namespace ShardMotion.Animation
             EditorUtility.SetDirty(clip);
         }
 
+        /// <summary>
+        /// Creates new animation clip
+        /// </summary>
+        /// <param name="assetPath">Where to save</param>
+        /// <param name="loop">Should the animation loop</param>
+        /// <returns></returns>
         public static AnimationClip CreateClipAsset(string assetPath, bool loop = false)
         {
             var clip = new AnimationClip { legacy = false };
 
             if (loop)
             {
-                var s = AnimationUtility.GetAnimationClipSettings(clip);
+                var s = AnimationUtility.GetAnimationClipSettings(clip); 
                 s.loopTime = true;
                 AnimationUtility.SetAnimationClipSettings(clip, s);
             }
-
+            
+            // clip created as asset at path
             AssetDatabase.CreateAsset(clip, assetPath);
             AssetDatabase.SaveAssets();
             return clip;
         }
 
+        /// <summary>
+        /// Add the keyframe as curve
+        /// </summary>
+        /// <param name="clip">Clip to add to</param>
+        /// <param name="binding">binding identifiing the property</param>
+        /// <param name="time">time in animation</param>
+        /// <param name="value">value in the keyframe</param>
         static void AddKey(AnimationClip clip, EditorCurveBinding binding, float time, float value)
         {
+            // Recomputes the curve depending on new key
             var curve = AnimationUtility.GetEditorCurve(clip, binding) ?? new AnimationCurve();
             InsertOrReplaceKey(curve, time, value);
             AnimationUtility.SetEditorCurve(clip, binding, curve);
         }
 
-        static void InsertOrReplaceKey(AnimationCurve curve, float t, float v)
+        /// <summary>
+        ///  inserts new keyframe to curve
+        /// </summary>
+        /// <param name="curve">curve to add to</param>
+        /// <param name="time">time in curve to add to</param>
+        /// <param name="value">vlaue of keframe</param>
+        static void InsertOrReplaceKey(AnimationCurve curve, float time, float value)
         {
+            // for each keyframe in curve
             for (int i = 0; i < curve.length; i++)
             {
-                if (Mathf.Approximately(curve.keys[i].time, t))
+                // if the time in curve is aproximately the same as time I should add in
+                if (Mathf.Approximately(curve.keys[i].time, time))
                 {
+                    // keyframe is already present at this time, replace
                     var k = curve.keys[i];
-                    k.value = v;
-                    curve.MoveKey(i, k);
+                    k.value = value;
+                    curve.MoveKey(i, k); // only way to modify existing curve keyframe
                     return;
                 }
             }
-            curve.AddKey(new Keyframe(t, v));
+            // add new keyframe to curve
+            curve.AddKey(new Keyframe(time, value));
         }
         #endif
     }
