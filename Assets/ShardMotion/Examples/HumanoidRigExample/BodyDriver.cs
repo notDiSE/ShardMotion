57d7d344f59c0d31faf3c7e08634cb55ecfc5d65
notDiSE <apribyl789@gmail.com>
2026-05-11 15:01:07 +0200

    HumanoidRigExample cleanup

--- Assets/ShardMotion/Examples/HumanoidRigExample/BodyDriver.cs
+++ Assets/ShardMotion/Examples/HumanoidRigExample/BodyDriver.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
 
-public class BodyDriver : MonoBehaviour
+namespace ShardMotion.Examples
 {
-    public Transform chestTracker;
-    public Transform chestBone;
-    public Vector3 chestOffset;
-
-    void LateUpdate()
+    /// <summary>
+    /// Component, that fixes the position of chest, while using it for tracking the whole body
+    /// </summary>
+    public class BodyDriver : MonoBehaviour
     {
-        if (chestTracker == null || chestBone == null) return;
-        
-        Vector3 boneToRoot = transform.position - chestBone.position;
-        
-        transform.position = chestTracker.position + boneToRoot + chestOffset;
-        transform.rotation = chestTracker.rotation;
+        /// <summary>
+        /// Reference to the object with ArUcoTarget(s)
+        /// </summary>
+        public Transform chestTracker;
+        /// <summary>
+        /// Reference to the body of chest in rig
+        /// </summary>
+        public Transform chestBone;
+        /// <summary>
+        /// Chest can be positionally offset
+        /// </summary>
+        public Vector3 chestOffset;
+
+        void LateUpdate()
+        {
+            if (chestTracker == null || chestBone == null) return;
+            
+            Vector3 boneToRoot = transform.position - chestBone.position; // the rig must be offset by the difference from the bone to origin
+            
+            transform.position = chestTracker.position + boneToRoot + chestOffset;
+            transform.rotation = chestTracker.rotation;
+        }
     }
+    
 }
